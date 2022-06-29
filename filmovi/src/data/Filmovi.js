07260130bf0ac2e797bd867c9b7bfa049f32c63f
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-29 15:43:59 +0200

    a

---
+++ filmovi/src/data/Filmovi.js
@@ -0,0 +1,38 @@
+ const Filmovi=[{
+    "title":"The Shawshank Redemption",
+    "img":"https://upload.wikimedia.org/wikipedia/sr/c/cb/The_Shawshank_Redemption.jpg",
+    "desc":"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "year":1994,
+    "rating":9.3,
+  },
+  {
+    "title":"The Godfather",
+    "img":"https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    "desc":"The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.",
+    "year":1972,
+    "rating":9.2,
+  },
+  {
+    "title":"One Flew Over the Cuckoo's Nest",
+    "img":"https://m.media-amazon.com/images/I/61ah-D5Hc9L._AC_SY679_.jpg",
+    "desc":"A criminal pleads insanity and is admitted to a mental institution, where he rebels against the oppressive nurse and rallies up the scared patients.",
+    "year":1975,
+    "rating":8.7,
+  },
+  {
+    "title":"Alien",
+    "img":"https://m.media-amazon.com/images/M/MV5BOGQzZTBjMjQtOTVmMS00NGE5LWEyYmMtOGQ1ZGZjNmRkYjFhXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg",
+    "desc":"The crew of a commercial spacecraft encounter a deadly lifeform after investigating an unknown transmission",
+    "year":1979,
+    "rating":8.5,
+  },
+  {
+    "title":"Parasite",
+    "img":"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
+    "desc":"Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
+    "year":2019,
+    "rating":8.5,
+  },
+];
+
+export default Filmovi;
