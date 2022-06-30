1eb341ba10d13cc9839f5dba9c9298e9452badb7
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-30 10:55:21 +0200

    a

--- filmovi/src/App.js
+++ filmovi/src/App.js
@@ -5,53 +5,53 @@ import { FaAward } from 'react-icons/fa';
 import { useState } from 'react';
 
 function App() {
-  const [trenutniFilm, podesiFilm]=useState(0)
-  
-const prev=()=>{
-let sada=trenutniFilm-1;
-if(sada<0){
-  sada=Filmovi.length-1;
-}
-podesiFilm(sada);
-}
-const next=()=>{
-  let sada=trenutniFilm+1;
-  if(sada>Filmovi.length-1){
-    sada=0;
+  const [trenutniFilm, podesiFilm] = useState(0);
+
+  const prev = () => {
+    let sada = trenutniFilm - 1;
+    if (sada < 0) {
+      sada = Filmovi.length - 1;
+    }
+    podesiFilm(sada);
   }
-  podesiFilm(sada);
+  const next = () => {
+    let sada = trenutniFilm + 1;
+    if (sada > Filmovi.length - 1) {
+      sada = 0;
+    }
+    podesiFilm(sada);
   }
- const randomFilm=()=>{
-return  Math.floor(Math.random()*Filmovi.length);
- }
-  const random=()=>{
+  const randomFilm = () => {
+    return Math.floor(Math.random() * Filmovi.length);
+  }
+  const random = () => {
     podesiFilm(randomFilm);
-    
+
   }
-const {img, title, desc, year, rating}=Filmovi[trenutniFilm];
+  const { img, title, desc, year, rating } = Filmovi[trenutniFilm];
   return (
     <div className="App">
       <div className="film">
         <div className="container">
-      <div className="card">
-  <img src={img} className="card-img-top" alt={title}/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{desc}</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item"><FcFilmReel/> snimljeno: {year}</li>
-    <li className="list-group-item"><FaAward/> ocena: {rating}</li>
+          <div className="card">
+            <img src={img} className="card-img-top" alt={title} />
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{desc}</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item"><FcFilmReel /> snimljeno: {year}</li>
+              <li className="list-group-item"><FaAward /> ocena: {rating}</li>
 
-  </ul>
-  <div className="card-body btn-group">
-<button className='btn btn-success btn-sm' onClick={prev}>Prethodni</button>
-<button className='btn btn-primary btn-sm' onClick={next}>Sledeći</button>
-  </div>
-  <button className='btn' onClick={random}>Daj nasumičan film</button>
-</div>      
-</div>
-</div>
+            </ul>
+            <div className="card-body btn-group">
+              <button className='btn btn-success btn-sm' onClick={prev}>Prethodni</button>
+              <button className='btn btn-primary btn-sm' onClick={next}>Sledeći</button>
+            </div>
+            <button className='btn' onClick={random}>Daj nasumičan film</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
